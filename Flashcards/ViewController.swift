d8be48fe80d4b5a8f97af429987e7a121fd8abe0
Jaelyn Domingo <jaelyndomingo@jaelyns-macbook-pro.local>
2018-10-27 16:03:37 -0700

    updated updateFlashcard

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -29,7 +29,8 @@ class ViewController: UIViewController {
     }
     
     func updateFlashcard(question: String, answer: String) {
-        
+        frontLabel.text = question
+        backLabel.text = answer
     }
    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
